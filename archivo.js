7a3eac0a3702ebb81e5c5b834cb9c69979f19bba
Illian27 <illianortegap@gmail.com>
2024-03-12 21:10:50 +0000

    Creacion carpeta imagenes e intento de modo oscuro

--- archivo.js
+++ archivo.js
@@ -1,1 +1,12 @@
-let hola
+// Variables disponibles
+let a,b,c;
+
+function modo(){
+    // Valores a utilizar
+    a = document.getElementById("body")
+
+    // Clases ha cambiar
+    a.classList.toggle("bg-dark")
+    a.classList.toggle("text-light")
+}
+document.getElementById("boton_modo").addEventListener("click", modo)
