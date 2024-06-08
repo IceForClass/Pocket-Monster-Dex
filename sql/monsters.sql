b98ba7c70f0ffef35f27148a0c4d5d8656e1e487
Icesito <113939920+Icesito68@users.noreply.github.com>
2024-06-08 19:47:12 +0100

    Inicio de base de datos añadido

---
+++ sql/monsters.sql
@@ -0,0 +1,15 @@
+create database pocket_monster;
+
+use pocket_monster;
+
+
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  username VARCHAR(45),
+  password VARCHAR(45),
+  tipo SET ('Usuario','Admin')
+);
+
+
+INSERT INTO usuarios(username, password, tipo) VALUES
+  ('admin', MD5('admin'), 'Admin');
