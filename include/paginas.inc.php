c3c1a8f289288536a02c026a28f4497e67bbcb64
Icesito <113939920+Icesito68@users.noreply.github.com>
2024-06-08 18:50:37 +0100

    Merge

---
+++ include/paginas.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+function headHTML()
+{
+    ?>
+    <!DOCTYPE paginas>
+
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Pocket monster dex</title>
+
+    <!--Links-->
+    <script src="https://kit.fontawesome.com/6b33bf05cd.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../js/archivo.js">
+    <link rel="stylesheet" href="../style/estilo.css">
+</head>
+<?php
+
+}
+?>
